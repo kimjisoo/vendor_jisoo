1c73691d2a5d1c6d3fc685d1ef9f90651375b288
kimjisoo <kimjisoo.android@gmail.com>
2015-05-13 10:51:19 +0900

    add gesture typing capabilities for 32 and 64-bit devices

--- config/common.mk
+++ config/common.mk
@@ -9,7 +9,10 @@ PRODUCT_COPY_FILES += \
     vendor/jisoo/prebuilt/common/app/FDroid/FDroid.apk:system/app/FDroid/FDroid.apk \
     vendor/jisoo/prebuilt/common/app/SlimLauncher/SlimLauncher.apk:system/app/SlimLauncher/SlimLauncher.apk \
     vendor/jisoo/prebuilt/common/app/Superuser/Superuser.apk:system/app/Superuser/Superuser.apk \
-    vendor/jisoo/prebuilt/common/bootanimation/bootanimation.zip:system/media/bootanimation.zip
+    vendor/jisoo/prebuilt/common/bootanimation/bootanimation.zip:system/media/bootanimation.zip \
+    vendor/jisoo/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so \
+    vendor/jisoo/prebuilt/common/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so \
+    vendor/jisoo/prebuilt/common/lib64/libjni_keyboarddecoder.so:system/lib64/libjni_keyboarddecoder.so
 
 # Additional packages
 -include vendor/jisoo/config/packages.mk
