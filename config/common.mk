cca144c5d8be49abb8bbed9eba81b99d754647c0
kimjisoo <kimjisoo.android@gmail.com>
2015-05-13 10:51:19 +0900

    add gesture typing capabilities for 32 and 64-bit devices

--- config/common.mk
+++ config/common.mk
@@ -13,8 +13,10 @@ PRODUCT_COPY_FILES += \
     vendor/jisoo/prebuilt/bin/backuptool.sh:system/bin/backuptool.sh \
     vendor/jisoo/prebuilt/bin/backuptool.functions:system/bin/backuptool.functions \
     vendor/jisoo/prebuilt/bin/50-hosts.sh:system/addon.d/50-hosts.sh \
-    vendor/jisoo/prebuilt/bin/blacklist:system/addon.d/blacklist
-
+    vendor/jisoo/prebuilt/bin/blacklist:system/addon.d/blacklist \
+    vendor/jisoo/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so \
+    vendor/jisoo/prebuilt/common/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so \
+    vendor/jisoo/prebuilt/common/lib64/libjni_keyboarddecoder.so:system/lib64/libjni_keyboarddecoder.so
   
 # Additional packages
 -include vendor/jisoo/config/packages.mk
