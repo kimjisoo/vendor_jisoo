4cb0cda42e7265db6aad2a1aaa64842c381e84fb
kimjisoo <kimjisoo.android@gmail.com>
2016-08-25 17:54:31 +0900

    initial commit

---
+++ config/common.mk
@@ -0,0 +1,29 @@
+# Selinux
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.selinux=1
+
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/jisoo/prebuilt/common/app/Amaze/Amaze.apk:system/app/Amaze/Amaze.apk \
+    vendor/jisoo/prebuilt/common/app/Etar/Etar.apk:system/app/Etar/Etar.apk \
+    vendor/jisoo/prebuilt/common/app/FDroid/FDroid.apk:system/app/FDroid/FDroid.apk \
+    vendor/jisoo/prebuilt/common/app/Fennec/Fennec.apk:system/app/Fennec/Fennec.apk \
+    vendor/jisoo/prebuilt/common/app/K9/K9.apk:system/app/K9/K9.apk \
+    vendor/jisoo/prebuilt/common/app/SlimLauncher/SlimLauncher.apk:system/app/SlimLauncher/SlimLauncher.apk \
+    vendor/jisoo/prebuilt/common/app/Superuser/Superuser.apk:system/app/Superuser/Superuser.apk \
+    vendor/jisoo/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so \
+    vendor/jisoo/prebuilt/common/lib64/libjni_latinimegoogle.so:system/lib64/libjni_latinimegoogle.so \
+    vendor/jisoo/prebuilt/common/lib64/libjni_keyboarddecoder.so:system/lib64/libjni_keyboarddecoder.so
+
+# Additional packages
+-include vendor/jisoo/config/packages.mk
+
+# Versioning
+-include vendor/jisoo/config/version.mk
+
+# APNs
+PRODUCT_COPY_FILES += \
+    vendor/jisoo/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# Dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/jisoo/overlay/dictionaries
---
+++ config/version.mk
@@ -0,0 +1,6 @@
+# Versioning of the ROM
+ ROM_VERSION := $(PLATFORM_VERSION)-$(shell date +%Y%m%d)-$(JISOO_RELEASE_TYPE)-$(subst jisoo_,,$(TARGET_PRODUCT))
+ JISOO_VERSION := $(PLATFORM_VERSION)-$(JISOO_RELEASE_TYPE)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.jisoo.version=$(ROM_VERSION)
